a34c200be693750c68a1d7bf3c5bc0d077015c30
HowieHepburn <2652740017@qq.com>
2022-10-05 21:58:22 +0800

    dynamic_broad

--- statictrans_pkg/scripts/static_broad_node.py
+++ statictrans_pkg/scripts/static_broad_node.py
@@ -7,21 +7,40 @@ from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import TransformStamped
 
 
+# 节点：坐标转换发布函数
+# 作用：发布子坐标系相对父坐标系的偏移关系，数据类型为TransformStamped
+# rosmsg info geometry_msgs/TransformStamped 
+# std_msgs/Header header
+#   uint32 seq
+#   time stamp
+#   string frame_id
+# string child_frame_id            #子坐标系的frame_id
+# geometry_msgs/Transform transform
+#   geometry_msgs/Vector3 translation
+#     float64 x
+#     float64 y
+#     float64 z
+#   geometry_msgs/Quaternion rotation
+#     float64 x
+#     float64 y
+#     float64 z
+#     float64 w
+
 if __name__ == "__main__":
-    rospy.init_node("tf_broad_node")
+    rospy.init_node("static_broad_node")
 
     broad = tf2_ros.StaticTransformBroadcaster()   #坐标变换专用数据发送函数
     
     broad_msg = TransformStamped()
 
-    broad_msg.header.frame_id = "world"  #坐标系id
+    broad_msg.header.frame_id = "world"  #父坐标系
     broad_msg.header.stamp = rospy.Time.now()  #时间戳
-    broad_msg.header.seq = 101  #序列号
+    broad_msg.header.seq = 20  #序列号
 
     broad_msg.child_frame_id = "laser" #子坐标系
-   
+
     # 子坐标系相对于父坐标系的偏移量
-    broad_msg.transform.translation.x = 0.2 
+    broad_msg.transform.translation.x = 2.0 
     broad_msg.transform.translation.y = 0.0
     broad_msg.transform.translation.z = 0.5 
 
